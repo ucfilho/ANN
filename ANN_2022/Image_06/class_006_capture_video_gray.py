3ec996d9becc8931e6bacd550e2772fce20d9ee9
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-10-11 12:57:41 -0300

    Add files via upload

---
+++ ANN_2022/Image_06/class_006_capture_video_gray.py
@@ -0,0 +1,16 @@
+import cv2
+print(cv2.__version__)
+
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while True:
+  ret,frame =cap.read()
+  gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+  cv2.imshow('frame',gray)
+  if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+cap.release()
+cv2.destroyAllWindows()
