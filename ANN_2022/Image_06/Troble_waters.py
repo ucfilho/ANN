24ae9c654d60a3e05b514fdbefa229978968528a
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-10-11 13:04:12 -0300

    Update and rename hello.py to Troble_waters.py

---
+++ ANN_2022/Image_06/Troble_waters.py
@@ -0,0 +1,8 @@
+# install opencv computer
+# 1) first install python
+# 2) install numpy, pandas, matplotlib
+# 3) install opencv: pip install opencv-python
+
+# note: Pressione a tecla Windows + R para abrir uma caixa de diálogo Executar. Em seguida, digite  cmd 
+#       echo% PATH% 
+#       Pressione a tecla Windows + R para abrir uma caixa de diálogo Executar. Em seguida, digite “ sysdm.cpl ” 
