f99eea7d14a067137c68d57044337b0657e1ea09
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-10-12 19:01:57 -0300

    Add files via upload

---
+++ ANN_2022/Image_06/class_006_open_video.py
@@ -0,0 +1,28 @@
+import cv2
+import os
+print(cv2.__version__)
+
+print(os.getcwd())
+path_dir = r"C:\Users\User\Documents\Atividades_andamento\computer_vision"
+os.chdir(path_dir)
+
+name_video = 'strawberry.mp4'
+cap = cv2.VideoCapture(name_video)
+
+if cap.isOpened() == False:
+  print('video not found')
+
+while cap.isOpened():
+  ret,frame =cap.read()
+
+  if ret == True:
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+      break
+  else:
+    break
+
+
+cap.release()
+cv2.destroyAllWindows()
