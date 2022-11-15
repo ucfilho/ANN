9b63eb6e69949957be3eea9ea85b4a6fb9cb72dc
ucfilho <42356381+ucfilho@users.noreply.github.com>
2022-11-14 21:03:27 -0300

    Create class_011_write_line_video.py

---
+++ ANN_2022/Image_11/class_011_write_line_video.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+print(cv2.__version__)
+
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+    else:
+        cv2.line(img=frame, pt1=(10, 400), pt2=(300, 400), color=(255, 0, 0), thickness=5, lineType=8, shift=0)
+
+vc.release()
+cv2.destroyWindow("preview")  
